b69eaaef593a3474d4b412f96260b4ef44dbc8b1
surkpopwan <popwandee@gmail.co>
2021-02-14 07:02:47 +0700

    update server path

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Bangkok');
 | a PHP script and you can easily do that on your own.
 |
 */
-$base  = "http://".$_SERVER['HTTP_HOST'];
+$base  = "https://morning-wave-06296.herokuapp.com/";
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base;
 
